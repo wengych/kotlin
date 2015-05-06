7aea30d1095b31b97fafaf499560816e9253bbc7
Valentin Kipyatkov <valentin@jetbrains.com>
2015-05-06 20:06:36 +0300

    KT-6780 Completion inserts incorrect comma in end of parameter list when lambda parameter exists
     #KT-6780 Fixed

--- idea/idea-completion/tests/org/jetbrains/kotlin/idea/completion/test/handlers/SmartCompletionHandlerTestGenerated.java
+++ idea/idea-completion/tests/org/jetbrains/kotlin/idea/completion/test/handlers/SmartCompletionHandlerTestGenerated.java
@@ -461,6 +461,12 @@ public class SmartCompletionHandlerTestGenerated extends AbstractSmartCompletion
         doTest(fileName);
     }
 
+    @TestMetadata("LastNonOptionalParamIsFunction.kt")
+    public void testLastNonOptionalParamIsFunction() throws Exception {
+        String fileName = JetTestUtils.navigationMetadata("idea/idea-completion/testData/handlers/smart/LastNonOptionalParamIsFunction.kt");
+        doTest(fileName);
+    }
+
     @TestMetadata("MergeTail1.kt")
     public void testMergeTail1() throws Exception {
         String fileName = JetTestUtils.navigationMetadata("idea/idea-completion/testData/handlers/smart/MergeTail1.kt");

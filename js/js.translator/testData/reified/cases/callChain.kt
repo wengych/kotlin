1ae35b5933978a0b737bbd8b2b317b739aec86c6
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2015-05-08 21:19:10 +0300

    ~ simplify call chain test

--- js/js.translator/testData/reified/cases/callChain.kt
+++ js/js.translator/testData/reified/cases/callChain.kt
@@ -4,32 +4,15 @@ package foo
 // CHECK_NOT_CALLED: test1
 
 class A
-
 class B
 
-class C
+inline fun test<reified T>(x: Any): Boolean = test1<T>(x)
 
-inline fun test<reified L, reified R>(x: Any): String = test1<R, L>(x)
-
-inline fun test1<reified R, reified L>(x: Any): String =
-        if (x is R) {
-            "R"
-        }
-        else if (x is L) {
-            "L"
-        }
-        else {
-            "Unknown"
-        }
+inline fun test1<reified R>(x: Any): Boolean = x is R
 
 fun box(): String {
-    val a: Any = A()
-    val b: Any = B()
-    val c: Any = C()
-
-    assertEquals("L", test<A, B>(a))
-    assertEquals("R", test<A, B>(b))
-    assertEquals("Unknown", test<A, B>(c))
+    assertEquals(true, test<A>(A()), "test<A>(A())")
+    assertEquals(false, test<A>(B()), "test<A>(B())")
 
     return "OK"
 }
